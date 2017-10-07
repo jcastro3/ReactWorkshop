4fd2c6e4d0b9e1bc9d1c396dbb7f0ced2aec6088
Jesus Castro <jcastro@nearsoft.com>
2017-10-07 03:22:26 -0700

    First Phase

--- App.js
+++ App.js
@@ -2,19 +2,47 @@
 
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
+import TextExample from './Components/TextExample'
+import ImageExample from './Components/ImageExample'
 import { 
   StyleSheet, 
   Text, 
   View, 
   Image,
   Button,
+  TouchableHighlight
 } from 'react-native';
 
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 12
+    flex: 1,
+    marginTop: 15,
+    flexDirection: 'column',
+    padding: 10
   },
+  logo: {
+    width: 200,
+    height: 200,
+    borderRadius: 100
+  },
+  welcomeContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    alignContent: 'flex-start'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+},
   picContainer: {
     display: 'flex',
     flexGrow: 1,
@@ -25,130 +53,65 @@ const styles = StyleSheet.create({
   picItem: {
     width: 200,
     height: 200
+  },
+  block1: {
+    flex: 1,
+    height: 100,
+    width: 100,
+    backgroundColor: 'powderblue'
+  },
+  block2: {
+    flex: 2,
+    height: 100,
+    width: 100,
+    backgroundColor: 'red'
+  },
+  block3: {
+    flex: 3,
+    height: 100,
+    width: 100,
+    backgroundColor: 'green'
+  },
+  block4: {
+    flex: 4,
+    height: 100,
+    width: 100,
+    backgroundColor: 'purple'
   }
 });
 
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
 
-class Welcome extends React.Component {
-  static navigationOptions = {
-    title: 'My First React App',
-    headerTintColor: 'red'
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <View>
-        <Text>Hello, im the Navigation</Text>
-        <Button
-          onPress={() => navigate('ChatScreen')}
-          title="My chat holis"/>
-        <Button
-          onPress={() => navigate('MyPics')}
-          title="My Pics"/>
-      </View>      
-    );
-  }
-};
-
-
-
-class ChatScreen extends React.Component {
-  static navigationOptions = {
-    title: 'My chat',
-    headerTintColor: 'blue'
-  };
-  render() {
-    return (
-      <View>
-        <Text> This is my chat box! :D </Text>
-      </View>      
-    );
-  }
-};
-
-class MyPics extends React.Component {
-  static navigationOptions = {
-    title: 'My Pic Files',
-    headerTintColor: 'green'
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
   };
   
-  printImages(images) {
-    return images.map((image, idx) => <Image key={idx} source={image} style={styles.picItem}/> );
-  }
-  
   render() {
-    let pics = [
-      { uri: 'http://cdn.cavemancircus.com//wp-content/uploads/images/2013/april/sloth/sloth_lol_14.jpg'},
-      { uri: 'http://www.qygjxz.com/data/out/193/5485088-random-image.png'},
-      { uri: 'https://www.newton.ac.uk/files/covers/968361.jpg'},
-      { uri: 'https://ugc.kn3.net/i/origin/http://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg'},
-      { uri: 'https://steamuserimages-a.akamaihd.net/ugc/780658011420073542/D0A3409F1A9ED4C370840B45E3AE48FC2A9D1C34/'}      
-    ];
-    const { navigate } = this.props.navigation;
+  let display = this.state.showText ? this.props.text : ' ';
     return (
-      <View style={styles.picContainer}>
-        {this.printImages(pics)}
-      </View>
-      )
-    }
-};
-
-const MyApp = StackNavigator({
-  Welcome: { screen: Welcome },
-  ChatScreen: { screen: ChatScreen },
-  MyPics: { screen: MyPics }
-});
-
-export default class App extends React.Component {
-  render() {
-    return <MyApp style={styles.container}/>;
+      <Text>{display}</Text>
+    );
   }
 }
 
-
-
-// export default class App extends React.Component {
-//   var navigationOptions = {
-//     title: 'Welcome'
-//   };
-//   render() {
-//     var  { navigate } = this.props.navigation;
-//     let pic = {
-//       uri: 'http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg'
-//     };
-//     
-//     let size = {
-//       w: 100,
-//       h: 100
-//     };
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.title}>Goobys pls</Text>
-//         <Doland pic={pic} width={size.w} height={size.h}></Doland>
-//         <Text> Have a nice day </Text>
-//       </View>
-//     );
-//   }
-// }
-// 
-// class Doland extends Component {
-//   render() {
-//     return (
-//       <Image source={this.props.pic} style={{ width: this.props.width, height: this.props.height}}/>
-//     );
-//   }
-// }
-// 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 30,
-//     color: 'red'
-//   }
-// });
+export default class App extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <TextExample></TextExample>
+        <ImageExample width={200} height={200}></ImageExample>
+        <Blink text='this should blink'/>
+        <View style={styles.block1}></View>
+        <View style={styles.block2}></View>
+        <View style={styles.block3}></View>
+        <View style={styles.block4}></View>
+      </View>
+    )
+  }
+}
 /* jshint ignore:end */
