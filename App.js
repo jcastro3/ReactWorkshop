851f602402049d31ddc0d3d10184591a6cacdd92
Jesus Castro <jcastro@nearsoft.com>
2017-10-07 12:58:09 -0700

    Intro demo

--- App.js
+++ App.js
@@ -2,10 +2,21 @@
 
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
-import { View } from 'react-native';
+import { 
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView
+ } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 12,
+    padding: 5,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   logo: {
     width: 200,
@@ -81,7 +92,50 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     backgroundColor: 'purple'
+  },
+  prettyfy: {
+    color: 'red',
+    fontSize: 34
   }
 });
 
+class Hola extends Component {
+    render() {
+      return(
+        <Text style={{ 'fontSize' : this.props.size }}>{this.props.text}</Text>
+      )
+    }
+}
+
+class PrintImages extends Component {
+  print(images) {
+    return images.map((_image, idx) => <Image key={idx} source={_image} style={styles.logo}/>)
+  }
+  render() {
+    return (
+      <View>
+        {this.print(this.props.images)}
+      </View>
+    )
+  }
+}
+
+export default class Welcome extends Component {
+  render() {
+    let pic = [
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"},
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"},
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"},
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"},
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"},
+      { uri: "https://thumbs.dreamstime.com/x/playful-three-toe-sloth-sitting-tree-costa-rica-12058189.jpg"}
+    ]
+    return (
+      <View style={styles.container}>
+        <Hola size={34} text={'Holis'} />
+        <PrintImages images={pic}/>
+      </View>
+    )
+  }
+}
 /* jshint ignore:end */
