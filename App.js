eb21682c68b2f8debe2bd32dd14dafb5225558fb
Jesus Castro <jcastro@nearsoft.com>
2017-10-07 10:21:19 -0700

    Blank slate

--- App.js
+++ App.js
@@ -5,13 +5,5 @@ import { StackNavigator } from 'react-navigation';
 import { View } from 'react-native';
 
 
-export   default class Welcome extends Component {
-  render() {
-    return(
-      <View>
-        <Text>Hola Mundo</Text>
-      </View>      
-    )
-  }
-}
+
 /* jshint ignore:end */
